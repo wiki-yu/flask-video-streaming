a18fbfb76a6217e96a7dce9b19e32cc2f48f8674
wiki-yu <xyu0127@gmail.com>
2021-01-28 09:20:29 -0600

    test

--- app.py
+++ app.py
@@ -1,46 +1,59 @@
 #!/usr/bin/env python
 from importlib import import_module
-import os
-from flask import Flask, render_template, Response
-import cv2
+import os,cv2
+from os import listdir
+from os.path import isfile, join
 import numpy as np
+from flask import Flask, render_template, Response, jsonify, request, send_from_directory, url_for
+import time
+from flask_cors import CORS
+
+from werkzeug.utils import secure_filename
+
+from urllib.request import urlopen
+
+from getmac import get_mac_address as gma
+import psutil
+import platform
+from datetime import datetime
+import GPUtil
+from markupsafe import escape
+import sqlite3 as sql
+
 
 # import camera driver
 if os.environ.get('CAMERA'):
     Camera = import_module('camera_' + os.environ['CAMERA']).Camera
 else:
-    from camera_opencv import Camera
+    from camera import Camera
 
 # Raspberry Pi camera module (requires picamera package)
 # from camera_pi import Camera
 
 app = Flask(__name__)
-
+# enable CORS
+CORS(app, resources={r'/*': {'origins': '*'}})
 
 @app.route('/')
 def index():
     """Video streaming home page."""
     print("step2")
-    return render_template('index.html')
-
+    # return render_template('index.html')
 
 def gen(camera):
     """Video streaming generator function."""
     print("step4")
     while True:
         frame = camera.get_frame()
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY ) 
-        # # aca5472-17uc
-        # width = 100
-        # height = 100
-        # dim = (width,height)
-        # frame = cv2.resize(frame, dim, interpolation=cv2.INTER_AREA) 
+        width = 5472//10
+        height = 3648//10
+        dim = (width,height)
+        frame = cv2.resize(frame, dim, interpolation=cv2.INTER_AREA) 
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-
-@app.route('/video_feed')
-def video_feed():
+@app.route('/motionDetection')
+def motionDetection():
     print("step3")
     """Video streaming route. Put this in the src attribute of an img tag."""
     return Response(gen(Camera()),
